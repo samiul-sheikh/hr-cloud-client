e7cac2602fbdd3011d802e34c72033dd19ccf41e
samiul-sheikh <sheikh.samiul1971@gmail.com>
2021-09-05 21:23:02 +0600

    addemployee and employeeinfo start with raw data

--- src/Component/AddEmployee/AddEmployee.js
+++ src/Component/AddEmployee/AddEmployee.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const AddEmployee = () => {
+
+    // const [firstName, setFirstName] = useState('')
+    // const [lastName, setLastName] = useState('')
     return (
-        <div>
-            <p>add emp</p>
+        <div className="container">
+            <h2>Add Employee</h2>
+            <div className="form">
+                <label>First Name</label>
+                <input type="text" name="firstName" />
+                <label>Last Name</label>
+                <input type="text" name="lastName" />
+                <label>Email Address</label>
+                <input type="text" name="email" />
+                <label>Phone No</label>
+                <input type="text" name="phone" />
+                <button type="button" class="btn btn-success">Submit</button>
+            </div>
         </div>
     );
 };
--- src/Component/EmployeeList/EmployeeList.js
+++ src/Component/EmployeeList/EmployeeList.js
@@ -2,8 +2,42 @@ import React from 'react';
 
 const EmployeeList = () => {
     return (
-        <div>
-            <p>emp list</p>
+        <div className="container">
+            <h6>Employee List</h6>
+            <table className="table table-bordered">
+                <thead className="thead-dark">
+                    <tr>
+                        <th scope="col">id</th>
+                        <th scope="col">First Name</th>
+                        <th scope="col">Last Name</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Phone No</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Samiul</td>
+                        <td>Sheikh</td>
+                        <td>sheikh.samiul1971@gmail.com</td>
+                        <td>01784097404</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">2</th>
+                        <td>Samiul</td>
+                        <td>Sheikh</td>
+                        <td>sheikh.samiul1971@gmail.com</td>
+                        <td>01784097404</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">3</th>
+                        <td>Samiul</td>
+                        <td>Sheikh</td>
+                        <td>sheikh.samiul1971@gmail.com</td>
+                        <td>01784097404</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     );
 };
