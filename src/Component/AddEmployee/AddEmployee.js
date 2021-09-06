3589352fda2c70ffe2619ee8b13269060ed12b74
samiul-sheikh <sheikh.samiul1971@gmail.com>
2021-09-06 23:54:25 +0600

    add employe feature done

--- src/Component/AddEmployee/AddEmployee.js
+++ src/Component/AddEmployee/AddEmployee.js
@@ -1,22 +1,34 @@
+import axios from 'axios';
 import React, { useState } from 'react';
 
 const AddEmployee = () => {
 
-    // const [firstName, setFirstName] = useState('')
-    // const [lastName, setLastName] = useState('')
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [email, setEmail] = useState('');
+    const [phone, setPhone] = useState('');
+
+    const submitInformation = () => {
+        // console.log("s");
+        axios.post("http://localhost:3000/api/insert", {id: id, firstName: firstName, lastName: lastName, email: email, phone: phone })
+            .then(() => {
+                alert("insert successfully");
+            });
+    };
+
     return (
         <div className="container">
             <h2>Add Employee</h2>
             <div className="form">
                 <label>First Name</label>
-                <input type="text" name="firstName" />
+                <input type="text" name="firstName" onChange={(e) => { setFirstName(e.target.value); }} />
                 <label>Last Name</label>
-                <input type="text" name="lastName" />
+                <input type="text" name="lastName" onChange={(e) => { setLastName(e.target.value); }} />
                 <label>Email Address</label>
-                <input type="text" name="email" />
+                <input type="text" name="email" onChange={(e) => { setEmail(e.target.value); }} />
                 <label>Phone No</label>
-                <input type="text" name="phone" />
-                <button type="button" class="btn btn-success">Submit</button>
+                <input type="text" name="phone" onChange={(e) => { setPhone(e.target.value); }} />
+                <button onClick={submitInformation} type="button" className="btn btn-success">Submit</button>
             </div>
         </div>
     );
